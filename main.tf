1c43ef48e8735bbcd4171f0334f8d4f1da28de0f
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2021-10-27 17:26:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,7 @@ variable "password_length" {
   default = 16
 }
 
-
-
-provider "random" {
-}
+provider "random" {}
 
 resource "random_string" "string" {
   length = var.password_length
@@ -14,4 +11,3 @@ resource "random_string" "string" {
 output "string" {
   value = random_string.string.result
 }
-
